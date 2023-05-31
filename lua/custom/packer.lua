cce74f589cdd5c4b30caf2f986d9b0038496bce1
Adrian du Plessis <adrian_dp@outlook.com>
2023-05-31 23:15:53 +0200

    Added nvim galaxyline pending configuration

--- lua/custom/packer.lua
+++ lua/custom/packer.lua
@@ -50,4 +50,15 @@ return require('packer').startup(function(use)
             require("nvim-tree").setup {}
         end
     }
+
+    use({
+        'glepnir/galaxyline.nvim',
+        branch = 'main',
+        -- your statusline
+        config = function()
+            require('my_statusline')
+        end,
+        -- some optional icons
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    })
 end)
