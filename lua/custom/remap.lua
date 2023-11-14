ec6c14bff2a02cc0a750ed53e7ed9cf972a5c086
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2023-11-14 15:02:26 +0100

    Telescope change with some new mappings

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = ' '
 --  This keymap is kept as a backup. Actual binding used is in nvim-tree.lua
 vim.keymap.set('n', '<leader>pv', vim.cmd.NvimTreeToggle)
+
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+--  Configure tab switching
+vim.keymap.set('n', '<C-[>', 'gT', {})
+vim.keymap.set('n', '<C-]>', 'gt', {})
