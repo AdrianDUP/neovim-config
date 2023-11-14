ab94eec8cc35eba9c17cc38dd78e9d651626d581
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2023-11-14 15:02:18 +0100

    Hopefully fixed indenting on PHP

---
+++ after/plugin/phpcs.lua
@@ -0,0 +1,3 @@
+vim.g.nvim_phpcs_config_phpcs_path='/home/adrian/.config/composer/vendor/bin/phpcs'
+vim.g.nvim_phpcs_config_phpcbf_path='/home/adrian/.config/composer/vendor/bin/phpcbf'
+vim.g.nvim_phpcs_config_phpcs_standard = 'PSR12'
