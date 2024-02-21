e708fb5519ee2d3c162c3b5bebf2fed395ae03cc
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-02-21 08:36:43 +0100

    Keymap tweaking and additional plugins

--- lua/custom/keymaps/keymaps.lua
+++ lua/custom/keymaps/keymaps.lua
@@ -23,22 +23,39 @@ vim.keymap.set('n', '<leader>be', function() require('neo-tree.command').execute
 vim.keymap.set('n', '<leader>c', require('cloak').toggle, { noremap = true })
 --  [[  UndoTree  ]]
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+--  [[ Trouble  ]]
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "<leader>gr", function() require("trouble").toggle("lsp_references") end)
 
 --  [[  Filesystem based Keymaps  ]]
 if vim.loop.os_uname().sysname == "Linux" then
+	--  [[  Harpoon  ]]
 	vim.keymap.set("n", "<A-a>", function() mark.add_file() end)
 	vim.keymap.set("n", "<A-e>", function() ui.toggle_quick_menu() end)
 	vim.keymap.set("n", "<A-h>", function() ui.nav_file(1) end)
 	vim.keymap.set("n", "<A-j>", function() ui.nav_file(2) end)
 	vim.keymap.set("n", "<A-k>", function() ui.nav_file(3) end)
 	vim.keymap.set("n", "<A-l>", function() ui.nav_file(4) end)
+	vim.keymap.set("n", "<A-H>", function() ui.nav_file(5) end)
+	vim.keymap.set("n", "<A-J>", function() ui.nav_file(6) end)
+	vim.keymap.set("n", "<A-K>", function() ui.nav_file(7) end)
+	vim.keymap.set("n", "<A-L>", function() ui.nav_file(8) end)
 else
+	--  [[  Harpoon  ]]
 	vim.keymap.set("n", "<leader>s", function() mark.add_file() end)
 	vim.keymap.set("n", "<leader>d", function() ui.toggle_quick_menu() end)
 	vim.keymap.set("n", "<leader>h", function() ui.nav_file(1) end)
 	vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
 	vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
 	vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
+	vim.keymap.set("n", "<leader>H", function() ui.nav_file(5) end)
+	vim.keymap.set("n", "<leader>J", function() ui.nav_file(6) end)
+	vim.keymap.set("n", "<leader>K", function() ui.nav_file(7) end)
+	vim.keymap.set("n", "<leader>L", function() ui.nav_file(8) end)
 end
 local tele = require('telescope.builtin')
 -- See `:help telescope.builtin`
@@ -52,7 +69,7 @@ vim.keymap.set("n", "<leader>/", function()
 	}))
 end, { desc = "[/] Fuzzily search in current buffer" })
 vim.keymap.set("n", "<leader>gf", tele.git_files, { desc = "Search [G]it [F]iles" })
-vim.keymap.set("n", "<leader>sa", function() tele.find_files({ find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden', '-u' } }) end)
+vim.keymap.set("n", "<leader>sa", function() tele.find_files({ find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden', '-u' } }) end, { desc = "[S]earch [A]ll files" })
 vim.keymap.set("n", "<leader>sf", tele.find_files, { desc = "[S]earch [F]iles" })
 vim.keymap.set("n", "<leader>sh", function() tele.find_files({hidden = true}) end, { desc = "[S]earch [H]idden" })
 vim.keymap.set("n", "<leader>sH", tele.help_tags, { desc = "[S]earch [H]elp" })
--- lua/custom/plugins/searching.lua
+++ lua/custom/plugins/searching.lua
@@ -18,10 +18,19 @@ return {
 				end,
 			},
 		},
-        priority = 900,
-        lazy = false,
+		priority = 900,
+		lazy = false,
 	},
 	{
-		'mbbill/undotree'
+		"mbbill/undotree",
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
 	},
 }
