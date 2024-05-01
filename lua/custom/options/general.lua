82583a9da5c1cec2a9a44468fe9b2be7b79e0458
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-05-01 18:14:47 +0200

    More options

--- lua/custom/options/general.lua
+++ lua/custom/options/general.lua
@@ -1,4 +1,19 @@
+--  Ensure tabs are using spaces
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+--  Line numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+--  Sync clipboard with os
+vim.o.clipboard = "unnamedplus"
+--  Break indent
+vim.opt.breakindent = true
+--  Undo history
+vim.opt.undofile = true
+--  Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+--  Sign column
+vim.wo.signcolumn = "yes"
