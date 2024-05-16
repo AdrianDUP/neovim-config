69adf6caf3506b359931158ec7f1c2b23c34ae03
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-05-16 19:20:55 +0200

    Reconfigured neovim, no lsp yet

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,5 +1,26 @@
 return {
     {
         "neovim/nvim-lspconfig",
-    }
+        dependencies = {
+            --  Mason for auto lsp installing
+            {
+                "williamboman/mason.nvim",
+                opts = {},
+            },
+            {
+                "williamboman/mason-lspconfig.nvim",
+                opts = {},
+            },
+            --  Autocomplete
+            {
+                "hrsh7th/nvim-cmp",
+                dependencies = {
+                    --  LSP Completion capabilities
+                    "hrsh7th/cmp-nvim-lsp",
+                },
+                opts = {},
+            },
+        },
+        priority = 750,
+    },
 }
