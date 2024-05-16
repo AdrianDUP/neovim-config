bc2a3b75080b82ef07e19963c58d4fef2c6afa59
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-05-16 08:37:50 +0200

    Added php unit to test squite

--- lua/custom/plugins/testing.lua
+++ lua/custom/plugins/testing.lua
@@ -7,11 +7,13 @@ return {
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-treesitter/nvim-treesitter",
 			'V13Axel/neotest-pest',
+			'olimorris/neotest-phpunit',
 		},
 		config = function()
 			require('neotest').setup({
 				adapters = {
 					require('neotest-pest'),
+					require('neotest-phpunit'),
 				},
 			})
 		end
