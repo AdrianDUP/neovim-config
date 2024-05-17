c009cd6137ff9c091acf11eda719ccd8546234c1
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-05-17 17:45:15 +0200

    Added some keymaps

--- lua/custom/keymaps/basic.lua
+++ lua/custom/keymaps/basic.lua
@@ -1,1 +1,11 @@
+--  [[  Split navigation  ]]
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+--  [[  Files  ]]
 vim.keymap.set("n", "-", "<CMD>Ex<CR>")
+--  [[  Conform keymap  ]]
+vim.keymap.set("n", "\\r", function()
+	require("conform").format()
+end)
