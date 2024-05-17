c67be3a8222f4796ed582675e092b1840471a9ee
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-05-17 22:34:30 +0200

    Updated keymaps, added some oil and tweaked the lsp config

--- lua/custom/keymaps/basic.lua
+++ lua/custom/keymaps/basic.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 --  [[  Files  ]]
-vim.keymap.set("n", "-", "<CMD>Ex<CR>")
+vim.keymap.set("n", "-", "<CMD>Oil<CR>")
 --  [[  Conform keymap  ]]
 vim.keymap.set("n", "\\r", function()
 	require("conform").format()
