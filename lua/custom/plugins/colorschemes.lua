ac6b0dfb73c79cb370f231452b4cf871683a5b94
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-10-14 16:04:53 +0200

    Custom overrides deleted

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -46,45 +46,6 @@ return {
 				which_key = true,
 			},
 		},
-		config = {
-			color_overrides = {
-				mocha = {
-					rosewater = "#f5e0dc",
-					pink = "#f5c2e7",
-					-- mauve = "#cba6f7",
-					maroon = "#eba0ac",
-					teal = "#94e2d5",
-					-- sapphire = "#74c7ec",
-					subtext1 = "#bac2de", --  Not sure what this is referring to
-					subtext0 = "#a6adc8", --  No idea what this is referring to
-					overlay2 = "#6c7086", --  Code completion default text color, also quotes, perenthases
-					overlay1 = "#7f849c", --  Code completion text highlight
-					overlay0 = "#6c7086", --  Comments and colors on popups text
-					surface2 = "#585b70", --  Cursor maybe
-					surface1 = "#45475a", --  Line numbers in the gutter
-					surface0 = "#313244", --  Current line indicator
-					mantle = "#181825", --  Middle part of the bottom bar
-					crust = "#11111b",
-					--
-					bg = "#0b0d0f",
-					base = "#0b0d0f", --  Background
-					fg = "#FFFFFF",
-					text = "#FFFFFF", --  Text color
-					blue = "#006AFF",
-					green = "#04D000",
-					--  lavender = "#6400FF",
-					lavender = "#9580FF",
-					red = "#FF0000",
-					yellow = "#F6F332",
-					flamingo = "#DF8B01",
-					sky = "#80FFEA", --  Command dialog border
-					peach = "#DF8B01", --  Command color and numbers and true/false
-					-- mauve = "#FF114F", --  Redish text in code and V-LINE and command
-					mauve = "#2596be", --  Redish text in code and V-LINE and command
-					sapphire = "#64C7FF"
-				},
-			},
-		}
 	},
 	{ "lunarvim/lunar.nvim" },
 	{
