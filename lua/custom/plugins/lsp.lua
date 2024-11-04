07bc71bd6e2b185585fa4c744e693d69a8d88fa0
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-11-04 08:38:22 +0100

    fixed LSP plugin definitions

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -15,7 +15,7 @@ return {
 			--  LSP Completion capabilities
 			"hrsh7th/cmp-nvim-lsp",
 			--  Snippet engine
-			"L3M0N4D3/LuaSnip",
+			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			--  Adds a number of user-friendly snippets
 			"rafamadriz/friendly-snippets",
