6944b5cfacb1772a5d6fb4e7693155cd004c23aa
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2025-01-26 20:31:57 +0100

    Added VS Code colorscheme

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,3 +1,10 @@
 return {
     { dir = "~/projects/dracula_pro", priority = 1000, lazy = false },
+	{
+		"askfiy/visual_studio_code",
+		priority = 100,
+		config = function()
+			vim.cmd([[colorscheme visual_studio_code]])
+		end,
+	},
 }
